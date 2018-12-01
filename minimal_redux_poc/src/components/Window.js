a5bca10ca72a843a7ebefa17f752ec8ecb4312fd
bodo22 <7796697+bodo22@users.noreply.github.com>
2018-12-01 16:00:12 +0100

    Add example use of focusWindow reducer
    Add explanation for redux devtools extension

--- minimal_redux_poc/src/components/Window.js
+++ minimal_redux_poc/src/components/Window.js
@@ -5,6 +5,8 @@ import fetch from 'node-fetch';
 import OpenSeaDragon from 'openseadragon';
 import ns from '../config/css-ns';
 
+import { focusWindow, } from '../actions';
+
 /**
  * Represents a Window in the mirador workspace
  * @param {object} window
@@ -68,7 +70,25 @@ class Window extends Component {
    * Return style attributes
    */
   styleAttributes() {
-    return { width: `${this.props.window.xywh[2]}px`, height: `${this.props.window.xywh[3]}px` };
+    let focusedStyle = {};
+    if (this.props.isFocused) {
+      focusedStyle = {
+        borderRadius: this.props.isFocused ? '20px' : '0px',
+        fontSize: '30px',
+        fontVariant: 'petite-caps',
+      }
+    }
+    // combine the current object with the potential focused styles
+    return Object.assign({
+        width: `${this.props.window.xywh[2]}px`,
+        height: `${this.props.window.xywh[3]}px`,
+      },
+      focusedStyle
+    );
+  }
+
+  focusWindow() {
+    this.props.focusWindow(this.props.window.id);
   }
 
   /**
@@ -77,7 +97,7 @@ class Window extends Component {
    */
   render() {
     return (
-      <div className={ns('window')} style={this.styleAttributes()}>
+      <div className={ns('window')} style={this.styleAttributes()} onClick={this.focusWindow.bind(this)}>
         <div className={ns('window-heading')}>
           <h3>{this.props.manifest.manifestation.getLabel().map(label => label.value)[0]}</h3>
         </div>
@@ -107,12 +127,25 @@ Window.defaultProps = {
  * @memberof Window
  * @private
  */
-const mapStateToProps = ({ windows, manifests }, props) => {
+const mapStateToProps = (state, props) => {
+  const windows = state.windows;
+  const manifests = state.manifests;
+  const workspace = state.workspace;
+  // getting the 3 parts of the reducer the "quicker" way would go like this:
+// const mapStateToProps = ({ windows, manifests, workspace, }, props) => {
+
   const window = windows.find(win => props.id === win.id);
   return {
     window,
     manifest: manifests[window.manifestId],
+    isFocused: window.id === workspace.focusedWindowId
   };
 };
 
-export default connect(mapStateToProps)(Window);
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    focusWindow: id => dispatch(focusWindow(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Window);
--- minimal_redux_poc/src/store.js
+++ minimal_redux_poc/src/store.js
@@ -13,6 +13,10 @@ import * as ActionCreators from './actions';
 // Its API is { subscribe, dispatch, getState }.
 export const store = createStore(
   rootReducer,
+  // the npm module used here enables the redux
+  // devtools extension, if its available
+  // see the package source code on github
+  // for the very few things it does
   composeWithDevTools(applyMiddleware(thunkMiddleware)),
 );
 
